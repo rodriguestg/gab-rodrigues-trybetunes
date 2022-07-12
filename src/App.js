e1d851a07402023a0e18db519acb3777e9d05cbf
rodriguestg <grs.ibr@gmail.com>
2022-07-12 00:32:00 -0300

    Subindo requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import Page404 from './pages/Page404';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>Oi TrybeTunes</p>);
+    return (
+      <>
+        <Route exact to="/" component={ Login } />
+        <Route to="/search" component={ Search } />
+        <Route to="/album/:id" component={ Album } />
+        <Route to="/favorites" component={ Favorites } />
+        <Route to="/profile" component={ Profile } />
+        <Route to="/profile/edit" component={ ProfileEdit } />
+        <Route to="/*" component={ Page404 } />
+      </>
+    );
   }
 }
 
---
+++ src/pages/Page404.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Page404 extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        a
+      </div>
+    );
+  }
+}
+
+export default Page404;
